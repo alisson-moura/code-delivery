97f5faed64109301cae20cf0a4e39d47220389b2
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-02-09 15:27:36 -0300

    feat: configurado MS na API

---
+++ api/src/routes/entities/route.entity.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, raw, SchemaFactory } from '@nestjs/mongoose';
+
+export type RouteDocument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      long: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; long: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      long: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; long: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
